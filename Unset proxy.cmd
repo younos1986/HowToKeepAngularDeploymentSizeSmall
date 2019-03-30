b822d085ce3fcb976d7c55ee4a749ef945d38417
Y.Baghaee <eng.younos1986@gmail.com>
2019-03-30 15:26:05 +0430

    asd

--- Unset proxy.cmd
+++ Unset proxy.cmd
@@ -1,2 +1,2 @@
-git config --global --unset http.proxy 127.0.0.1:8580
-git config --global --unset https.proxy 127.0.0.1:8580
+git config --global --unset http.proxy 127.0.0.1:9666
+git config --global --unset https.proxy 127.0.0.1:9666
