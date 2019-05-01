e48d9ab26be33e7fd3107b4bb863f598aa33ddd7
Y.Baghaee <eng.younos1986@gmail.com>
2019-05-01 16:44:07 +0430

    imagesAdded

--- src/app/projects/dashboard/pages/users/users.component.ts
+++ src/app/projects/dashboard/pages/users/users.component.ts
@@ -34,7 +34,7 @@ export class UsersComponent implements OnInit {
 
   getUsers()
   {
-    this._userService.getUsers().subscribe(
+    this._userService.getUsers('').subscribe(
       res=>
       {
         console.log(res);
