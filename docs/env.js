490460d7947121f5e15242b9f914f1eaad2237bd
Y.Baghaee <eng.younos1986@gmail.com>
2019-05-02 10:52:02 +0430

    new_docs

---
+++ docs/env.js
@@ -0,0 +1,13 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.PacsApi = 'http://192.168.5.70:90';
+  // window.__env.PacsApi = 'https://localhost:44333';
+
+  
+  
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = true;
+}(this));
